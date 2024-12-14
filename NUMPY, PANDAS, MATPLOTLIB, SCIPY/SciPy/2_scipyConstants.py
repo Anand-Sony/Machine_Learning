56075343555add1bb6fd5e48a68cf30a2132db84
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-12-14 12:29:02 +0530

    Add files via upload

---
+++ NUMPY, PANDAS, MATPLOTLIB, SCIPY/SciPy/2_scipyConstants.py
@@ -0,0 +1,23 @@
+from scipy import constants
+
+light_speed = constants.speed_of_light
+gravitational_Constant = constants.gravitational_constant
+boltzmannConstant = constants.k
+
+print("Speed of light :", light_speed)
+print("Gravitational Constant :", gravitational_Constant)
+print("Boltzmann Constant :", boltzmannConstant)
+
+pi = constants.pi
+golden_Ratio = constants.golden_ratio
+
+print(pi)
+print(golden_Ratio)
+
+mileToMeter = constants.mile
+inchto_meter = constants.inch
+knot_to_mps = constants.knot
+
+print(mileToMeter)
+print(inchto_meter)
+print(knot_to_mps)
